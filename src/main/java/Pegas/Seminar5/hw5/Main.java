5374693af5828f627bb0aee3b9acb8c8e95cf226
Dioneer <gulkina_em@mail.ru>
2024-03-25 23:33:37 +0300

    debug

--- src/main/java/Pegas/Seminar5/hw5/Main.java
+++ src/main/java/Pegas/Seminar5/hw5/Main.java
@@ -1,7 +1,5 @@
 package Pegas.Seminar5.hw5;
 
-import java.util.concurrent.CountDownLatch;
-
 public class Main {
         public static void main(String[] args) {
             Table table = new Table();
